c945fa18dd376f8c75b1b6c6a54dc1f07eb556e1
feng-plus <1725909232@qq.com>
2020-03-24 23:07:40 +0800

    branch2 commit java

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello{
+   public static void main(string[] args){
+      System.out.println("2");
+      System.out.println("ni hao java");
+      System.out.println("1");
+      System.out.println("3");
+   }   
+  
+}
